1d26e16a8bc092be1d300a221ab38c543a3112b9
Davi Hebert <davihebert017@gmail.com>
2025-09-14 18:47:41 -0300

    deploy gh-pages

--- frontend-produtos-mobile/vite.config.js
+++ frontend-produtos-mobile/vite.config.js
@@ -1,10 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/frontend-produtos-mobile/', // <- use /NOME-DO-REPO/
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+  server: { port: 5173, open: true }
 })
